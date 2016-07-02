e817f8a31fa397c294c1e1a5ca4f992879865414
bozidarrr <bozidarrr@gmail.com>
2016-07-02 17:00:43 +0200

    Added encoding of values, to reduce the memory footprint in case of long variable names.

--- DP_Procedure/Literal.cpp
+++ DP_Procedure/Literal.cpp
@@ -1,35 +1,109 @@
 #include "Literal.h"
+#include<iostream>
+#include <algorithm>
 
-Literal::Literal(const std::string & var, const bool value) : var(var), value(value)
+unsigned char Literal::s_nextCode = 1;
+std::map<std::string, std::pair<unsigned char, unsigned int> > Literal::s_NamesDictionary = std::map<std::string, std::pair<unsigned char, unsigned int> >();
+std::map <unsigned char, std::string> Literal::s_variablesDictionary = std::map <unsigned char, std::string>();
+
+Literal::Literal(const std::string & name, const bool value) : _value(value)
 {
+	if (s_NamesDictionary.find(name) == s_NamesDictionary.end())
+	{
+		_code = s_nextCode++;
+		s_NamesDictionary[name] = std::make_pair(_code,1);
+		s_variablesDictionary[_code] = name;
+	}
+	else
+	{
+		_code = s_NamesDictionary[name].first;
+		s_NamesDictionary[name].second++;
+	}
 }
 
-Literal::Literal(const Literal & literal) : var(literal.var), value(literal.value)
-{
-}
+Literal::Literal(const Literal & literal) : Literal(literal.getName(), literal.getValue())
+{}
 
 Literal::~Literal()
 {
+	std::string name = getName();
+	int count = --s_NamesDictionary[name].second;
+	if (0 == count)
+	{
+		s_NamesDictionary.erase(name);
+		s_variablesDictionary.erase(getCode());
+	}
 }
 
-Literal& Literal::operator=(const Literal& other) 
+Literal& Literal::operator=(const Literal& other)
 {
-	if (this != &other) 
-	{ 
-		var = other.var;
-		value = other.value;
+	if (this != &other)
+	{
+		_code = other.getCode();
+		_value = other.getValue();
+		std::string name = other.getName();
+		s_NamesDictionary[name].second++;
 	}
+
 	return *this;
+}
+
+bool Literal::operator<(const Literal& other) const
+{
+	return getCode() < other.getCode() ||
+		(
+		getCode() == other.getCode() &&
+		!getValue() && other.getValue()
+		);
+}
+
+bool Literal::operator<=(const Literal& other) const
+{
+	return *this < other || *this == other;
+}
+
+bool Literal::operator>(const Literal& other) const
+{
+	return getCode() > other.getCode() ||
+		(
+		getCode() == other.getCode() &&
+		getValue() && !other.getValue()
+		);
+}
+
+bool Literal::operator>=(const Literal& other) const
+{
+	return  *this > other || *this == other;
+}
+
+bool Literal::operator==(const Literal& other) const
+{
+	return getCode() == other.getCode() && getValue() == other.getValue();
+}
+
+std::string Literal::getName() const
+{
+	return s_variablesDictionary[getCode()];
+}
+
+unsigned char Literal::getCode() const
+{
+	return _code;
+}
+
+bool Literal::getValue() const
+{
+	return _value;
 }
 
 Literal Literal::getOpposite() const
 {
-	return Literal(var, !value);
+	return Literal(getName(), !getValue());
 }
 
 bool Literal::isPositive() const
 {
-	return value == true;
+	return getValue() == true;
 }
 
 bool Literal::isNegative() const
@@ -37,45 +111,15 @@ bool Literal::isNegative() const
 	return !isPositive();
 }
 
-const std::string & Literal::getVariable() const
-{
-	return var;
-}
-
 bool Literal::isOppositeOf(const Literal & other) const
 {
-	return (var == other.var) && (value != other.value);
-}
-
-bool Literal::operator<(const Literal& other) const
-{
-	return (var < other.var) || (var == other.var && !value && other.value);
-}
-bool Literal::operator<=(const Literal& other) const
-{
-	return *this < other || *this == other;
-}
-bool Literal::operator>(const Literal& other) const
-{
-	return (var > other.var) || (var == other.var && value && !other.value);
-}
-bool Literal::operator>=(const Literal& other) const
-{
-	{
-		return  *this > other || *this == other;
-	}
-}
-bool Literal::operator==(const Literal& other) const
-{
-	{
-		return var == other.var && value == other.value;
-	}
+	return (getCode() == other.getCode()) && (getValue() != other.getValue());
 }
 
 std::ostream &operator<<(std::ostream &output, const Literal & l)
 {
-	if (!l.value)
+	if (!l.getValue())
 		output << "!";
-	output << l.var;
+	output << l.getName();
 	return output;
 }
--- DP_Procedure/Literal.h
+++ DP_Procedure/Literal.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <ostream>
+#include <set>
+#include <map>
 
 /**
 Klasa koja omogucava predstavljanje literala.
@@ -11,9 +13,27 @@ Negacija se predstavlja prefiksom '!'.
 class Literal
 {
 public:
+	/**
+		Mapa koja omogucava brzu pretragu postojanja koda za literal sa datim imenom
+		Ime literala se preslikava u par koji cine njegov kod (koji je sacuvan u samoj instanci)
+		i broj pojavljivanja tog literala
+	*/
+	static std::map<std::string, std::pair<unsigned char, unsigned int> > s_NamesDictionary;
+
+	/**
+		Mapa koja omogucava brzo pronalazenje imena literala, na osnovu njegovog koda
+	*/
+	static std::map <unsigned char, std::string> s_variablesDictionary;
+
+	/**
+		Sledeci kod koji ce biti upotrebljen pri kreiranju instance.
+	*/
+	static unsigned char s_nextCode;
+
 	Literal(const std::string & var, const bool value);
 	Literal(const Literal & literal);
 	~Literal();
+	
 	/**
 		Naredni operatori poredjenja su neophodni da bi se koristila struktura set<Literal>. 
 	*/
@@ -23,23 +43,49 @@ public:
 	bool operator>(const Literal& other) const;
 	bool operator>=(const Literal& other) const;
 	bool operator==(const Literal& other) const;
+	
 	/**
-		Metod vraca suprotan literal. 
+		Metod vraca ime literala, na osnovu podataka iz staticke mape.
 	*/
-	Literal getOpposite() const;
+	std::string getName() const;
+	
+	/**
+		Metod vraca kod datog literala.
+	*/
+	unsigned char getCode() const;
+
+	/**
+		Metod koji vraca istinitosnu vrednost datog literala
+	*/
+	bool getValue() const;
+
 	/**
 		Metod vraca vrednost 'true' ako je u pitanju pozitivan literal, a vrednost 'false' ako je negativan.
 	*/
 	bool isPositive() const;
+	
 	/**
-	Metod vraca vrednost 'false' ako je u pitanju pozitivan literal, a vrednost 'true' ako je negativan.
+		Metod vraca vrednost 'false' ako je u pitanju pozitivan literal, a vrednost 'true' ako je negativan.
 	*/
 	bool isNegative() const;
+	
+	/**
+		Metod vraca vrednost 'true' ukoliko je trenutni literal suprotan datom, inace 'false'.
+	*/
 	bool isOppositeOf(const Literal & other) const;
-	const std::string & getVariable() const;
+
+	/**
+	Metod vraca suprotan literal.
+	*/
+	Literal getOpposite() const;
+
+	/**
+		Metod koji sluzi za ispis datog literala.
+	*/
 	friend std::ostream &operator<<(std::ostream &output, const Literal & l);
+
 private:
-	std::string var;
-	bool value;
+	unsigned char _code;
+	bool _value;
 };
 
